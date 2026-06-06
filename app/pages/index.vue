41cc06a8016b8c8795619254fae78926ec3d8416
Ahmad MKH <apolonsoft@gmail.com>
2026-06-06 16:41:09 +0330

    install and configure tailwindcss

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,22 @@
+<script lang="ts" setup>
+useHead({
+  title: 'My simple product catalog'
+})
+</script>
+
 <template>
   <header>
     <nav>
       <ul>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/products/1">Product 1</NuxtLink></li>
-        <li><NuxtLink to="/products/2">Product 2</NuxtLink></li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products/1">Product 1</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products/2">Product 2</NuxtLink>
+        </li>
       </ul>
     </nav>
   </header>
